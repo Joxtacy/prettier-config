9a3c22800e2126b33be88c3d94b4a5c397f98d18
Jesper Hasselquist <jesper.hasselquist@telavox.se>
2021-02-09 01:37:18 +0900

    Add Prettier rules

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // Styles
+    arrowParens: "always",
+    bracketSpacing: true,
+    endOfLine: "lf",
+    printWidth: 80,
+    proseWrap: "preserve",
+    quoteProps: "as-needed",
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "es5",
+    useTabs: false,
+    // Options
+    embeddedLanguageFormatting: "auto",
+    htmlWhitespaceSensitivity: "css",
+    insertPragma: false,
+    requirePragma: false,
+};
